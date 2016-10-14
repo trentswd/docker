cc666801186990bd600da754761e30c177043474
root <root@trent2.b4xilt5ninxezbp2ad12ulkode.hx.internal.cloudapp.net>
2016-10-14 06:31:30 +0000

    use latest aria2c, which is static linked.

--- aria2/run.sh
+++ aria2/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-aria2c --dir=/data --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all
+aria2c --dir=/data --conf-path=/etc/aria2/aria2.conf  --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all
